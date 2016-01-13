5a2f7645623693b0478f9fcbab5ad0f23f0e3f11
yurialexsander <yuriatp@gmail.com>
2016-01-12 21:36:37 -0300

    Recalcula a velocidade da bola de acordo com a posição de colisão dela com a barra. Implementa a tela de fim de jogo.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,32 @@
+//VERIFICAR A CALIBRAGEM DO ADVERSÁRIO
+
 var canvas;
 var cContext;
 
-var ballPositionX = 10;
-var ballPositionY = 10;
-var ballSpeedX = 6;
-var ballSpeedY = 6;
+var ballPositionX = 50;
+var ballPositionY = 50;
+var ballSpeedX = 10;
+var ballSpeedY = 4;
 
 var player01 = {
+	name: "PLAYER O1",
 	score : 0,
 	positionY : 250
 }
 var player02 = {
+	name: "PLAYER 02",
 	score : 0,
 	positionY : 250
 }
+
+var finishGame = false;
+var winner = "";
 
 const FRAMES_PER_SECOND = 30;
 const BALL_DIMENSIONS = 15;
 const PLAYER_WIDTH = 10;
 const PLAYER_HEIGHT = 100;
-
-
-function getMousePosition (evt) {
-	var gameRect = canvas.getBoundingClientRect();
-	var root = document.documentElement;
-	//CALCULA A POSICAO DO MOUSE EM RELACAO À CAIXA DO JOGO
-	var mouseX = evt.clientX - gameRect.left - root.scrollLeft;
-	var mouseY = evt.clientY - gameRect.top - root.scrollTop;
-
-	return { x: mouseX, y: mouseY }
-}
+const WINNING_SCORE = 1;
 
 window.onload = function () {
 	
@@ -37,12 +34,19 @@ window.onload = function () {
 	cContext = canvas.getContext("2d");
 	
 	setInterval(function(){
-		updatePositions();
-		drawElements();
+		if (finishGame) {
+			drawEndScreen();
+			return;
+		} else {
+			updatePositions();
+			drawElements();
+		}
 	}, 1000/FRAMES_PER_SECOND);
 
+	//CRIA O LISTENER DE MOVIMENTO DO MOUSE
 	canvas.addEventListener("mousemove", function(evt) {
 		var mousePosition = getMousePosition(evt);
+		//POSICIONA O MOUSE NO MEIO DA BARRA DO JOGADOR
 		player01.positionY = mousePosition.y - (PLAYER_HEIGHT/2);
 	});
 
@@ -74,8 +78,9 @@ function drawElements () {
 	drawRect(0, player01.positionY, PLAYER_WIDTH, PLAYER_HEIGHT, "white");
 	drawRect(canvas.width-PLAYER_WIDTH, player02.positionY, PLAYER_WIDTH, PLAYER_HEIGHT, "white");
 	drawRect(ballPositionX, ballPositionY, BALL_DIMENSIONS, BALL_DIMENSIONS, "white");
-	cContext.fillText(player01.score, 100, 100);
-	cContext.fillText(player02.score, canvas.width-100, 100);
+	cContext.font = "40px 'Press Start 2P'";
+	cContext.fillText(player01.score, 130, 100);
+	cContext.fillText(player02.score, canvas.width-160, 100);
 }
 
 function drawRect (customLeft, customTop, customWidth, customHeight, customColor) {
@@ -83,16 +88,35 @@ function drawRect (customLeft, customTop, customWidth, customHeight, customColor
 	cContext.fillRect(customLeft, customTop, customWidth, customHeight);
 }
 
+function getMousePosition (evt) {
+	var gameRect = canvas.getBoundingClientRect();
+	var root = document.documentElement;
+	//CALCULA A POSICAO DO MOUSE EM RELACAO À CAIXA DO JOGO
+	var mouseX = evt.clientX - gameRect.left - root.scrollLeft;
+	var mouseY = evt.clientY - gameRect.top - root.scrollTop;
+
+	return { x: mouseX, y: mouseY }
+}
+
 function checkCollision (player) {
 	if ((ballPositionY + (BALL_DIMENSIONS/2)) > player.positionY && 
 		(ballPositionY + (BALL_DIMENSIONS/2)) < (player.positionY + PLAYER_HEIGHT)) {
 
 		ballSpeedX = -ballSpeedX;
+		//RECALCULA A VELOCIDADE DA BOLA NO EIXO Y
+		//DE ACORDO COM A POSIÇÃO DE COLISÃO DELA COM A BARRA
+		recalculateBallSpeedY(player);
 
 	} else {
-	
 		playerScored(player);
 	}
+}
+
+function recalculateBallSpeedY (player) {
+	//VERIFICA ONDE (EM RELAÇÃO AO CENTRO DA BARRA) HOUVE A COLISÃO DA BOLA
+	var ballCollision = ballPositionY - (player.positionY + (PLAYER_HEIGHT/2));
+
+	ballSpeedY = ballCollision * 0.35;
 }
 
 function opponentMovement () {
@@ -107,16 +131,36 @@ function opponentMovement () {
 }
 
 function playerScored (player) {
-
 	//SE O PLAYER 01 É PARÂMETRO DA FUNÇÃO
 	//SIGNIFICA O ADVERSÁRIO FOI QUEM PONTUOU
 	if (player == player01) {
 		player02.score++;
+		checkTheEndOfTheGame(player02);
 	} else {
 		player01.score++;
+		checkTheEndOfTheGame(player01);
 	}
+}
 
+function checkTheEndOfTheGame (player) {
+	if (player.score >= WINNING_SCORE) {
+		finishGame = true;
+		winner = player.name;
+	}
+	//REPOSICIONA A BOLA NO CENTRO DA TELA
 	ballSpeedX = -ballSpeedX;
 	ballPositionX = canvas.width/2;
 	ballPositionY = canvas.height/2;
+}
+
+function drawEndScreen () {
+	drawRect(0, 0, canvas.width, canvas.height, "black");
+	cContext.fillStyle = "white";
+	cContext.textAlign = "center";
+	cContext.font = "44px 'Press Start 2P'";
+	cContext.fillText("FIM DE JOGO", canvas.width/2, 175);
+	cContext.font = "20px 'Press Start 2P'";
+	cContext.fillText("VITÓRIA DO " + winner + "!", canvas.width/2, 220);
+	cContext.font = "16px 'Press Start 2P'";
+	cContext.fillText("JOGAR NOVAMENTE", canvas.width/2, 380);
 }
